2419d401238b53753ccd4db6053bc9a618e6f090
曹江川 <384682535@qq.com>
2020-02-25 22:50:37 +0800

    打包

---
+++ my-app/build/precache-manifest.b871c5124aa4335bbf4e9d92fc1cc2bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2426004762715ab267f1ac09b33d7b75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3737d75c38be1a5ff44d",
+    "url": "./static/css/main.f7741d39.chunk.css"
+  },
+  {
+    "revision": "2edc49804e21debf49e9",
+    "url": "./static/js/2.7b7e6665.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.7b7e6665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3737d75c38be1a5ff44d",
+    "url": "./static/js/main.d1cbd960.chunk.js"
+  },
+  {
+    "revision": "3744c28cdaff03dd305e",
+    "url": "./static/js/runtime-main.14c24d16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
